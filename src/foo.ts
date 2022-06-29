9167dbafcc602c1c9bd1360a130e6aaacd88829b
hiromish0 <hiromish@fsi.co.jp>
2022-06-29 15:03:02 +0900

    HTML追加

---
+++ src/foo.ts
@@ -0,0 +1,18 @@
+class Person {
+	name: string;
+	age: number;
+				
+	constructor(name: string, age: number){
+		this.name = name;
+		this.age = age;
+	}
+											
+	sayHello(){
+		return `My name is ${this.name}. I'm ${this.age} years old.`;
+	}
+} 
+
+let p = new Person('Mike', 25);
+let s = p.sayHello();
+
+
